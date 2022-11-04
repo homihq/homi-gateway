105f1539c237b3853eb42686af6143e2b35d6a4f
grabdoc <grabdoc2020@gmail.com>
2022-11-04 09:13:26 -0500

    Initial commit

---
+++ src/main/java/com/homihq/homigateway/HomiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.homihq.homigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomiGatewayApplication.class, args);
+    }
+
+}
